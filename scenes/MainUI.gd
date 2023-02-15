4e5e87280c706ae1a0c4fbbdc7b3739e052df2f9
Steven <sclausson@gmail.com>
2023-02-14 20:14:28 -0700

    Update 2
    - Timber Camp working as intended.
    - Research Button added.
    - Nerfed Forage()
    - Buffed Timber Camp
    - Days last 3 seconds now.
    - Primitive Well working as intended.
    - Small House working as intended.
    - Family Farm working as intended.
    - Large house working as intended.
    - Added standard tooltips to buildings(hints).

--- scenes/MainUI.gd
+++ scenes/MainUI.gd
@@ -6,11 +6,12 @@ onready var water = Global.water
 onready var timber = Global.timber
 onready var stone = Global.stone
 onready var day = Global.day
+onready var year = Global.year
 
 func _ready():
 	var timer = Timer.new()
 	add_child(timer)
-	timer.set_wait_time(5)
+	timer.set_wait_time(3)
 	timer.connect("timeout", self, "_on_Timer_timeout")
 	timer.start()
 
@@ -18,11 +19,11 @@ func UpdateUIData():
 	$TopUIPanel/PopulationLbl.text = "Population: " + str(Global.population)
 	$TopUIPanel/FoodLbl.text = "Food: " + str(Global.food)
 	$BottomUIPanel/TimberSprite/TimberLbl.text =": " + str(Global.timber)
-	$TopUIPanel/DayLbl.text = "Day: " + str(day)
+	$TopUIPanel/DayLbl.text = "Day: " + str(Global.day)
 	$BottomUIPanel/StoneSprite/StoneLbl.text = ": " +str(Global.stone)
 	$TopUIPanel/UnemployedLbl.text = "Unemployed: " +str(Global.unemployed)
 	$TopUIPanel/WaterLbl.text = "Water: " + str(Global.water)
-
+	$TopUIPanel/YearLbl.text = "Year: " + str(Global.year)
 func _on_Timer_timeout():
 	EndDay()
 	Global.PopulationAppetite()
@@ -32,8 +33,16 @@ func _on_Timer_timeout():
 	UpdateUIData()
 
 func EndDay():
-	day += 1
+	day += 10
+	if (day > 365):
+		year += 1
+		day = 0
 	$BuildingPanel/TimberCampLbl/Button.disabled = false
+	$BuildingPanel/PrimWellLbl/BuildPWellBtn.disabled = false
+	$BuildingPanel/SmallHouseLbl/BuildSmallHouseBtn.disabled = false
+	$BuildingPanel/FamFarmLbl/BuildFamFarmBtn.disabled = false
+	$BuildingPanel/LargeHouseLbl2/BuildLHouseBtn.disabled = false
+	$BuildingPanel/StoneQuarryLbl/BuildQuarryBtn.disabled = false
 	print("Another day has passed...")
 
 func _on_ForageBtn_pressed():
@@ -45,3 +54,31 @@ func _on_Button_pressed(): #Timber Camp Build
 	Global.BuildTimberCamp()
 	$BuildingPanel/TimberCampLbl/Button.disabled = true
 	$BuildingPanel/TimberCampLbl/Label.text = str(Global.timberCamp)
+
+func _on_BuildPWellBtn_pressed():
+	Global.BuildPrimitiveWell()
+	$BuildingPanel/PrimWellLbl/BuildPWellBtn.disabled = true
+	$BuildingPanel/PrimWellLbl/PWellTextLbl.text = str(Global.primitiveWell)
+
+func _on_BuildSmallHouseBtn_pressed():
+	Global.BuildSmallHouse()
+	$BuildingPanel/SmallHouseLbl/BuildSmallHouseBtn.disabled = true
+	$BuildingPanel/SmallHouseLbl/SmallHouseTxt.text = str(Global.smallHouse)
+
+
+func _on_BuildFamFarmBtn_pressed():
+	Global.BuildFamilyFarm()
+	$BuildingPanel/FamFarmLbl/FamFarmTxtLbl.text = str(Global.familyFarm)
+	$BuildingPanel/FamFarmLbl/BuildFamFarmBtn.disabled = true
+
+
+func _on_BuildLHouseBtn_pressed():
+	Global.BuildLargeHouse()
+	$BuildingPanel/LargeHouseLbl2/BuildLHouseBtn.disabled = true
+	$BuildingPanel/LargeHouseLbl2/LHouseTxtLbl.text = str(Global.largeHouse)
+
+
+func _on_BuildQuarryBtn_pressed():
+	Global.BuildStoneQuarry()
+	$BuildingPanel/StoneQuarryLbl/BuildQuarryBtn.disabled = true
+	$BuildingPanel/StoneQuarryLbl/SQuarryTextLbl.text = str(Global.stoneQuarry)
--- scripts/Global.gd
+++ scripts/Global.gd
@@ -4,24 +4,32 @@ var rng = RandomNumberGenerator.new()
 
 
 var day = 0
-
+var year = 0
 
 var population = 10
 var unemployed = 0 
 var populationGrowthRate = 0.008
 
 var food = 100
+var foodCollectionRate = 0
+var isStarving = false
+var starvingDays = 0
+
 var water = 300
+var waterCollectionRate = 0
 
 var timber = 10
 var timberGrowthRate = 0
 var stone = 10
+var stoneCollectionRate = 0
 var hide = 20
 
-var simpleHut = 0
+var smallHouse = 0
+var largeHouse = 0
+var familyFarm = 0
 var timberCamp = 0
-var stoneCamp = 0
-
+var stoneQuarry = 0
+var primitiveWell = 0
 
 
 
@@ -32,34 +40,99 @@ var stoneCamp = 0
 
 func Forage():
 	rng.randomize()
-	timber += rng.randf_range(1,4)
-	food += population * 0.01 + rng.randf_range(0,10)
-	water += population * 0.01 + rng.randf_range(0,10)
+	timber += rng.randf_range(0,1)
+	stone += rng.randf_range(0,2)
+	food += population * 0.01 + rng.randf_range(0,5)
+	water += population * 0.01 + rng.randf_range(0,5)
 
 func PopulationAppetite():
 	food -= population * 3
 	water -= population * 4
+	if (food <=0 ):
+		food = 0
+		isStarving = true
+		starvingDays += 1
+		print("Your people are starving my lord.")
+	else:
+		isStarving = false
+	if (water <= 0):
+		water == 0
 
 func PopulationGrowth():
 	if (population >= 2):
 		population += populationGrowthRate
-		print("Population grew to " + str(population))
 	else:
 		pass
 
+#----------------------------------------
+# ------ BUILDING FUNCTIONS
+#---------------------------------------
 func BuildTimberCamp():
-	if(unemployed >= 1) and (timber >= 2) and (stone >= 2):
+	if(unemployed >= 1) and (timber >= 65) and (stone >= 65):
 		timberCamp +=1
 		timberGrowthRate += 1
+		timber -= 65
+		stone -= 65
+
+func BuildStoneQuarry():
+	if(timber >= 40) and (unemployed >= 1):
+		stoneQuarry += 1
+		stoneCollectionRate += 2
+		timber -= 40
+
+func BuildPrimitiveWell():
+	if(timber >= 45) and (stone >= 100):
+		primitiveWell += 1
+		waterCollectionRate += 10
+		timber -= 45
+		stone -= 100
+
+func BuildSmallHouse():
+	if (smallHouse <= population):
+		if (timber >= 225) and (stone >= 120):
+			smallHouse += 1
+			populationGrowthRate += 0.002
+			timber -= 225
+			stone -= 120
+		else:
+			pass
+	else:
+		pass
+
+func BuildFamilyFarm():
+	if(familyFarm <= population):
+		if(timber >= 100) and (stone >= 75):
+			familyFarm += 1
+			foodCollectionRate += 10
+			timber -= 100
+			stone -= 75
+		else:
+			pass
+	else:
+		pass
 		
-
-
-
-
+func BuildLargeHouse():
+	if (largeHouse <= population):
+		if (timber >= 450) and (stone >= 250):
+			largeHouse += 1
+			populationGrowthRate += 0.004
+			timber -= 450
+			stone -= 250
+		else:
+			pass
+	else:
+		pass
+#-----------------------------------------
 
 func ResourceCollection():
-	timber += timberGrowthRate
+	timber += (timberGrowthRate + rng.randf_range(1,5))
+	stone += (stoneCollectionRate + rng.randf_range(1,8))
+	water += (waterCollectionRate + rng.randf_range(3,8))
+	food += (foodCollectionRate + rng.randf_range(1,3))
+	
+
+
 func CheckEmployment():
-	unemployed = population - (timberCamp + stoneCamp)
+	unemployed = population - (timberCamp + stoneQuarry)
 #UI STUFF
 
